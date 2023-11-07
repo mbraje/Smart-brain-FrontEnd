b46852a7a11db48cae0030e715ca540edca2606e
mbraje <121793051+mbraje@users.noreply.github.com>
2023-11-07 22:37:14 +0100

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -16,7 +16,7 @@ class Signin extends React.Component  {
   }
 
   onSubmitSignIn = () => {
-    fetch("https://smartbrain-sj9r.onrender.com/signin", {
+    fetch("https://smartbrainbackend-zj6z.onrender.com/signin", {
       method: "post",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({
